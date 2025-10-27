f83dba1dc35c5e7c4ee740b15d6bf7e18e2f0261
Alla-Eddine BOUKABOU <alaatheprince25@gmail.com>
2025-10-27 09:19:44 +0100

    Turning the timer function into a component + Fixing the issue with the timer animation

---
+++ Game/src/components/Timer.vue
@@ -0,0 +1,125 @@
+<script setup>
+  import { useTimer, useStopwatch } from 'vue-timer-hook';
+  import { onMounted,watchEffect, computed } from 'vue';
+const props = defineProps({
+    timeLimit:{
+        type: Number,
+        required: true
+    }
+});
+ 
+  const time = new Date();
+  time.setSeconds(time.getSeconds() + props.timeLimit);
+
+  const chrono = useTimer(time)
+
+  onMounted(() => {
+  watchEffect(async () => {
+    if(chrono.isExpired.value) {
+        console.log('The timer has expired')
+    }
+  })
+})
+  const totalLength = 2 * Math.PI * 45;
+
+  const timeFraction = computed(() =>{
+  const fraction =  (chrono.seconds.value)/ props.timeLimit;
+  const adjFraction = fraction -(1 / props.timeLimit) * (1-fraction);
+//   Pour éviter que ça renvoie une valeur négative
+  return Math.max(0,  adjFraction);
+});
+
+  const elapsedDash = computed(() => (timeFraction.value * totalLength))
+
+
+</script>
+
+<template>
+    <div>
+  <h1>Chrono : </h1>
+   <div class="container">
+    <svg class="svg" viewBox="0 0 100 100">
+      <g class="circle">
+        <circle class="time-elapsed-path" cx="50" cy="50" r="45" />
+
+         <path
+          class="time-left-path"
+          d="
+            M 50, 50
+            m -45, 0
+            a 45,45 0 1,0 90,0
+            a 45,45 0 1,0 -90,0
+          "
+          :style="{  strokeDasharray: `${elapsedDash} ${totalLength}` }"
+        ></path>
+
+      </g>
+    </svg>
+
+    <div class="time-left-container">
+      <span>{{ chrono.seconds }}</span>
+    </div>
+  </div>
+</div>
+</template>
+
+<style scoped>
+    .container {
+  height: 150px;
+  width: 150px;
+  position: relative;
+}
+
+.circle {
+  fill: none;
+  stroke: none;
+}
+
+.time-elapsed-path {
+  stroke-width: 7px;
+  stroke: #424242;
+}
+
+.time-left-container {
+  /* Size should be the same as that of parent container */
+  height: inherit;
+  width: inherit;
+
+  /* Place container on top of circle ring */
+  position: absolute;
+  inset: 0;
+
+  /* Center content (label) vertically and horizontally  */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  font-size: 70px;
+  color: white;
+}
+
+.time-left-path {
+  /* Same thickness as the original ring */
+  stroke-width: 7px;
+  /* Colors the ring */
+  stroke: rgb(255, 0, 0);
+  /* Rounds the path endings  */
+  stroke-linecap: round;
+
+  /* Makes sure the animation starts at the top of the circle */
+  transform: rotate(90deg);
+  transform-origin: center;
+
+  /* One second aligns with the speed of the countdown timer */
+  /* transition: 1s linear all; */
+  transition: stroke-dasharray 1s linear, stroke 0.3s ease;;
+
+  
+}
+
+.svg {
+  /* Flips the svg and makes the animation to move left-to-right */
+  transform: scaleX(-1);
+}
+ 
+</style>
