b29871bcd9b5754bbaaa1704e44e4722d1b472fb
Alla-Eddine BOUKABOU <alaatheprince25@gmail.com>
2026-03-20 08:47:35 +0100

    Fixing the deployment issues

--- Game/vite.config.js
+++ Game/vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/1Question_4Answers_Game',
+  base: '/',
   plugins: [
     vue(),
     vueDevTools(),
